0033550e94a94e535475725ac1b8b967c8528d1d
David <zomhorsethompson2656@gmail.com>
2024-02-29 19:09:32 -0700

    added onCLick for chips

--- Public/BlackJack/js/BlackJack.js
+++ Public/BlackJack/js/BlackJack.js
@@ -1,17 +1,32 @@
 import Game from './Game.js'
 let game
+let bet
 
 document.getElementById('actionButton').addEventListener("click", ()=>{
-    event.preventDefault()
-    game = new Game(0)
+    ev.preventDefault()
+    game = new Game(bet)
 })
 
-document.getElementById('hitbutton').addEventListener("click", ()=>{
-    event.preventDefault()
+document.getElementById('hitbutton').addEventListener("click", (ev)=>{
+    ev.preventDefault()
     game.hit()
 })
 
-document.getElementById('doubleDownButton').addEventListener("click", ()=>{
-    event.preventDefault()
+document.getElementById('doubleDownButton').addEventListener("click", (ev)=>{
+    ev.preventDefault()
     game.doubleDown()
+})
+
+document.getElementById('chip').addEventListener("click", (ev)=>{
+    ev.target.preventDefault()
+    const button = ev.target
+    for(buttonClass in button.classList){
+        try{
+            let buttonValue = parseInt(buttonClass)
+            bet += buttonValue
+        }
+        catch(err){
+            continue
+        }
+    }
 })
