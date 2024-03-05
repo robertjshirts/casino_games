33252076f8f13913360c0df746864944e67174f6
Breeves03 <breeves@student.neumont.edu>
2024-03-05 00:29:15 -0700

    added reset and fixed buttons

--- Public/Roulette/js/Display.js
+++ Public/Roulette/js/Display.js
@@ -6,12 +6,24 @@ var cursorY = 0
 const actionButton = document.getElementById('actionButton');
 const outcomeElement = document.getElementById('outcome');
 
-actionButton.addEventListener('click', ()=>{
+document.getElementById('reset').addEventListener('click', () => {
+    const elements = document.querySelectorAll('.clone');
+    bets = []
+    elements.forEach(element => {
+        element.remove()
+    });
+})
+
+var t = setInterval(function () {
+    document.getElementById("cash").innerHTML = "Cash: $" + localStorage.cash;
+}, 200);
+
+actionButton.addEventListener('click', () => {
     var element = document.getElementById('wheel');
     element.classList.add('spinning');
     console.log('working')
-    setTimeout(function() {
-      element.classList.remove('spinning');
+    setTimeout(function () {
+        element.classList.remove('spinning');
     }, 4000); // 3000 milliseconds = 3 seconds
 })
 
@@ -32,7 +44,7 @@ const zeroHitBox = document.getElementById("zero");
 
 
 
-const hitBoxes = [firstDozenHitBox,secondDozenHitBox,thirdDozenHitBox,bottomRowHitBox,middleRowHitBox,topRowHitBox,bottomHalfHitBox,upperHalfHitBox,evenHitBox,oddHitBox,redHitBox,blackHitBox,zeroHitBox]
+const hitBoxes = [firstDozenHitBox, secondDozenHitBox, thirdDozenHitBox, bottomRowHitBox, middleRowHitBox, topRowHitBox, bottomHalfHitBox, upperHalfHitBox, evenHitBox, oddHitBox, redHitBox, blackHitBox, zeroHitBox]
 
 
 var boardHitBox = board.getBoundingClientRect();
@@ -72,14 +84,14 @@ document.addEventListener('mousedown', function (e) {
                 'chip5000'
             ];
             let chipClass = null
-        
+
             for (var i = 0; i < classesToCheck.length; i++) {
                 if (clone.classList.contains(classesToCheck[i])) {
                     chipClass = classesToCheck[i];
                     break;
                 }
             }
-        
+
             switch (chipClass) {
                 case 'chip1':
                     amount = 1
@@ -108,25 +120,28 @@ document.addEventListener('mousedown', function (e) {
                 case 'chip5000':
                     amount = 5000
                     break;
-        
             }
-        
+
+            if (amount > Number(localStorage.cash)) {
+                clone.remove()
+            }
+
             document.removeEventListener('mousemove', move);
-            let x = cursorX +0
-            let y = cursorY +0
+            let x = cursorX + 0
+            let y = cursorY + 0
             if (x >= boardHitBox.left && x <= boardHitBox.right && y >= boardHitBox.top && y <= boardHitBox.bottom) {
 
-            x = cursorX - boardHitBox.left
-            y = cursorY - boardHitBox.top
-            let snapX = (Math.floor(x / 40) * 41) + 13;
-            let snapY = (Math.floor(y / 55) * 57) + 15
-            clone.style.left = snapX + boardHitBox.left
-            clone.style.top = snapY + boardHitBox.top
+                x = cursorX - boardHitBox.left
+                y = cursorY - boardHitBox.top
+                let snapX = (Math.floor(x / 40) * 41) + 13;
+                let snapY = (Math.floor(y / 55) * 57) + 15
+                clone.style.left = snapX + boardHitBox.left
+                clone.style.top = snapY + boardHitBox.top
 
-            addBasicBet(x, y, clone, amount)
+                addBasicBet(x, y, clone, amount)
 
-            }else{
-                addComplexBet(x,y,clone, amount)
+            } else {
+                addComplexBet(x, y, clone, amount)
             }
 
         });
@@ -138,9 +153,9 @@ document.addEventListener('mousedown', function (e) {
 //24 columns
 //6 rows        
 
-function addComplexBet(x,y,element,amount){
-let location = null
-    hitBoxes.every((e) =>{
+function addComplexBet(x, y, element, amount) {
+    let location = null
+    hitBoxes.every((e) => {
         let box = e.getBoundingClientRect()
         if (x >= box.left && x <= box.right && y >= box.top && y <= box.bottom) {
             location = e.id;
@@ -148,18 +163,18 @@ let location = null
         }
         return true;
     })
-    if(location == null){
-        element.style.visibility = 'hidden'
+    if (location == null) {
+        element.remove()
         return;
-    }else{
+    } else {
         bets.push({
-            'type':'complex',
-            'bet':location,
-            'amount':amount
+            'type': 'complex',
+            'bet': location,
+            'amount': amount
 
         })
     }
-    
+
     console.log(bets)
 }
 
@@ -167,18 +182,18 @@ function addBasicBet(x, y, element, amount) {
 
     let col = Math.floor((x / 40))
     if (col > 23 || col < 0) {
-        element.style.visibility = 'hidden'
+        element.remove()
         return;
     }
 
     let row = Math.floor(y / 55)
     if (row > 5 || row < 0) {
-        element.style.visibility = 'hidden'
+        element.remove()
         return;
     }
 
     let bet = {
-        'type':"basic",
+        'type': "basic",
         'row': row,
         'col': col,
         'amount': amount
@@ -197,19 +212,19 @@ function addBasicBet(x, y, element, amount) {
 // }
 
 // special Enum
-    // firstDozen,
-    // secondDozen,
-    // thirdDozen,
-    // bottomRow,
-    // middleRow,
-    // topRow,
-    // bottomHalf,
-    // upperHalf,
-    // even,
-    // odd,
-    // red,
-    // black,
-    // zero,
+// firstDozen,
+// secondDozen,
+// thirdDozen,
+// bottomRow,
+// middleRow,
+// topRow,
+// bottomHalf,
+// upperHalf,
+// even,
+// odd,
+// red,
+// black,
+// zero,
 
 /////
 
